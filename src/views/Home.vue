fd5ac8a091209c768e62fd0f4f69910d8c0a6810
Pitt Wu <kgb00128@gmail.com>
2020-12-27 22:39:27 +0800

    initial vue3 project & setting prettier rules & install plugin

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.home
+  img(alt="Vue logo" src="../assets/logo.png")
+  HelloWorld(msg="Welcome to Your Vue.js + TypeScript App")
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
