1fcd4835a7c06f802d8a33cf3a9ffb0a2fb0bf24
pitt.wu <pitt.wu@jh-tech.tw>
2021-01-15 23:42:26 +0800

    feat：add icon

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
 ul(class="row")
-  li(class="col-4", v-for="item in list", :key="item.id")
+  li(class="col-4 mb-4", v-for="item in columnList", :key="item.id")
     div(class="card h-100 shadow-sm")
-      div(class="card-body text-center")
+      div(class="card-body")
         img(class="rounded-circle border border-light w-25 my-3" :src="item.avatar", :alt="item.title")
         h5(class="card-title") {{ item.title }}
         p(class="card-text") {{ item.description }}
-        a(class="btn btn-primary", href="#") Go Column
+        a(class="btn btn-outline-primary", href="#") Go Column
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
 
@@ -29,7 +29,17 @@ export default defineComponent({
   },
 
   setup(props) {
-    props.list[0].description;
+    const columnList = computed(() => {
+      return props.list.map((node) => {
+        // if not found image url use local static image
+        if (!node.avatar) {
+          node.avatar = require('@/assets/column.jpg');
+        }
+        return node;
+      });
+    });
+
+    return { columnList };
   },
 });
 </script>
