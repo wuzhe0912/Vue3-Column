1831734fbe1818245b619bb6b6d35b6f182da997
Pitt Wu <pitt.wu@hlwtech.com.tw>
2022-02-10 17:47:46 +0800

    feat: add temp content

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
-      Hello world!
-    </h1>
+    <div className='app'>
+      <header className=''>
+        <h1 className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
+          Hello world!
+        </h1>
+      </header>
+      <main>content</main>
+      <footer>footer</footer>
+    </div>
   );
 }
 
