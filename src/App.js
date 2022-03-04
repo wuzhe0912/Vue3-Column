3d8318c191ab4c9a3a1f9011903eb4aa33f5f314
Pitt Wu <kgb00128@gmail.com>
2022-03-04 23:43:49 +0800

    feat: update data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import showCaseData from './constants/data';
 
 function App() {
+  console.log(1, showCaseData);
+
   return (
     <div className='app'>
       <header className=''>
         <h1 className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
-          Hello world!
+          Pitt Wu's Showcase App
         </h1>
       </header>
       <main>content</main>
--- src/constants/data.js
+++ src/constants/data.js
@@ -1,11 +1,37 @@
 const data = [
+  // {
+  //   filterTag: '',
+  //   tags: [],
+  //   name: '',
+  //   image_url: '',
+  //   link_url: '',
+  //   source_code: '',
+  // },
   {
-    uuid: '',
-    tags: [],
-    name: '',
-    image_url: '',
-    link_url: '',
-    source: '',
+    filterTag: 'React',
+    tags: ['React', 'Docusaurus'],
+    name: 'Personal Docusaurus Blog',
+    image_url:
+      'https://raw.githubusercontent.com/wuzhe0912/image-save/master/showcase-app/docusaurus-blog-preview.dyyxkedvhs0.webp',
+    link_url: 'https://pitt-wu-blog.vercel.app',
+    source_code: 'https://github.com/wuzhe0912/blog-docusaurus',
+  },
+  {
+    filterTag: 'Vue',
+    tags: ['Vue.js 3.x', 'Tailwind CSS', 'Leaflet'],
+    name: 'IP Address Tracker',
+    image_url:
+      'https://raw.githubusercontent.com/wuzhe0912/image-save/master/20211228/ip-tracker-vue3.190qettga5r4.webp',
+    link_url: 'https://ip-tracker-vue3.vercel.app',
+    source_code: 'https://github.com/wuzhe0912/ip-tracker-vue3',
+  },
+  {
+    filterTag: 'React',
+    tags: ['React', 'Styled Components'],
+    name: 'Todos App(React)',
+    image_url: 'https://i.imgur.com/ND42RNK.gif',
+    link_url: 'https://todos-app-react.vercel.app',
+    source_code: 'https://github.com/wuzhe0912/todos-app-react',
   },
 ];
 
